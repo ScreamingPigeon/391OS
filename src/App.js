88d9cd8a59dcec667de71bee4158e17e62feefd4
MeanPaper <ldm1833991255@gmail.com>
2023-05-20 21:48:31 -0500

    create navigation bar and normal layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Outlet, Link } from "react-router-dom";
-import V86Container from './components/V86Container';
-import bios from './emulatorFiles/seabios.bin'
-import vgabios from './emulatorFiles/vgabios.bin'
-import mp3 from './osImg/mp3.img'
-import v86wasm from './emulatorFiles/v86.wasm'
+
+import Layout from "./views/Layout";
+import Home from './views/Home';
+import Demo from './views/Demo';
+import Contact from './views/Contact';
+import About from './views/About';
 
 function App() {
   return (
@@ -29,7 +29,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
+          <Route path="demo" element={<Demo />} />
+          <Route path="about" element={<About />}/>
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
@@ -41,37 +42,6 @@ function App() {
 export default App;
 
 
-const Layout = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-    </>
-  )
-};
-const Home = () => {
-  return <h1><V86Container biosURL={bios} vgaURL={vgabios} hdaURL={mp3} wasmPath={v86wasm}/>
-  </h1>;
-};
-const Blogs = () => {
-  return <h1>Blog Articles</h1>;
-};
-const Contact = () => {
-  return <h1>Contact Me</h1>;
-};
 const NoPage = () => {
   return <h1>404</h1>;
 };
