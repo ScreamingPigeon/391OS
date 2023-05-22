a21ad7fffd8af3f800cd439eb27de8d44d9c22bd
MeanPaper <ldm1833991255@gmail.com>
2023-05-22 00:11:02 -0500

    basic dark mode is implement

--- src/views/NoPage.js
+++ src/views/NoPage.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const NoPage = () => {
-    return(<div className="content-404">
+    return(<div className=" content-container content-404">
         <h1>OOPS! Nothing is here!</h1>
         <div className="redirect-button"><Link to='/'> Go Back To Home </Link></div>
     </div>);
