555dca5167a861330cb5ebd4786827b772231369
MeanPaper <ldm1833991255@gmail.com>
2023-05-25 17:47:12 -0500

    breathing / fading effect

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,33 @@
 import logo from '../hopeforcpp.svg'
+import { useState, useEffect } from 'react';
 
-const descriptionItem = 'home-description-item';
-// const descriptionTextArray = ['We are team Hope for a Cpp', 
-//                               'A team from Fall 2022 of ECE 391', 
-//                               'A team of people who know how to use Cpp but forgot how to use C ',
-//                               'That is why we Hope for a Cpp',
-//                               'Yet, we made it to the end and created this simple Linux OS',
-//                               '391OS'
-//                             ];
+const carouselItems = ['We are team Hope for a Cpp', 
+                              'A team from Fall 2022 of ECE 391', 
+                              'A team of people who know how to use Cpp but forgot how to use C ',
+                              'That is why we Hope for a Cpp',
+                              'Yet, we made it to the end and created this simple Linux OS',
+                              '391OS'
+                            ];
 
+const BreathingCarousel = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const timeInMs = 6000; 
+    const animationStyle = { animation: `breathing-animation ${timeInMs / 1000}s infinite`};
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % carouselItems.length);
+        }, timeInMs);
+    
+        return () => clearInterval(interval); // Clean up the interval on component unmount
+    }, []);
+    
+    return (
+        <div className="carousel-container">
+            <div className='carousel-item' style={animationStyle}> {carouselItems[activeIndex]}</div>
+        </div>
+    );
+};
 
 const Home = () => {
     
@@ -16,14 +35,7 @@ const Home = () => {
         <div className="home-box">
             <div id='homepage-welcome'> Welcome </div>
             <img id='homepage-large-logo' src={logo} alt='hope for a cpp logo' />
-            <div id='homepage-description'>
-                <div className={descriptionItem}> We are team Hope for a Cpp </div>
-                <div className={descriptionItem}> A team from Fall 2022 of ECE 391</div>
-                <div className={descriptionItem}> A team of people who know how to use Cpp but forgot how to use C </div>
-                <div className={descriptionItem}> That is why we Hope for a Cpp </div>
-                <div className={descriptionItem}> Yet, we made it to the end and created this simple Linux OS </div>
-                <div className={descriptionItem}> 391OS </div>
-            </div>
+            <BreathingCarousel />
             <div className='road-map'> Road map </div>
         </div>
     </div>);
--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,35 +1,47 @@
 import { Outlet, Link } from "react-router-dom";
+// import { useEffect, useRef, useState } from 'react';
 import hopeforcpp from "../hopeforcpp.svg";
 
 const Layout = () => {
-    return (
-      <>
-        <div className="nav-header">
-          <div className="nav-item home-logo-outer">
-            <Link to="/" className="home">
-              <img src={hopeforcpp} className='home-logo' alt="hope for a cpp team logo" />
-            </Link>
-          </div>
-          <div className="nav-item">
-            <Link to="/demo">
-              <div className="link-item" >Demo</div>
-            </Link>
-          </div>
-          <div className="nav-item">
-            <Link to="/about">
-              <div className="link-item">About</div>
-            </Link>
-          </div>
-          <div className="nav-item">
-            <Link to="/contact">
-              <div className="link-item">Contact</div>
-            </Link>
-          </div>
+  
+  // const navigationBarRef = useRef();
+  // useEffect(()=>{
+  //   const scollHandler = (entries) => {  
+  //     const isVisible = entries[0].isIntersecting;
+  //     console.log(isVisible);
+  //   }
+  //   const observer = new IntersectionObserver(scollHandler);
+  //   observer.observe(navigationBarRef.current);
+  // });
+
+  return (
+    <>
+      <div className="nav-header">
+        <div className="nav-item home-logo-outer">
+          <Link to="/" className="home">
+            <img src={hopeforcpp} className='home-logo' alt="hope for a cpp team logo" />
+          </Link>
         </div>
-      
-        <Outlet />
-      </>
-    )
+        <div className="nav-item">
+          <Link to="/demo">
+            <div className="link-item" >Demo</div>
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/about">
+            <div className="link-item">About</div>
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/contact">
+            <div className="link-item">Contact</div>
+          </Link>
+        </div>
+      </div>
+    
+      <Outlet />
+    </>
+  )
 };
 
 export default Layout;
