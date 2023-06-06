62dc5f52a3ecffcda7db41179c2ef15e07cac27c
MeanPaper <ldm1833991255@gmail.com>
2023-06-06 01:36:49 -0500

    modify about page

--- src/views/About.js
+++ src/views/About.js
@@ -1,5 +1,11 @@
+const AboutNavigation = () => {
+    return (<div className='about-nav'>
+    </div>)
+}
+
 const About = () => {
     return (<div className="content-container">
+        <AboutNavigation />
         <div id='about-box'>
             <div id='about-page-title'>About</div>
             <div>
@@ -27,23 +33,22 @@ const WebDevInfo = () => {
             <div className='wed-dev-info'>
                 <h3> Web Developments:</h3>
                 <div> The online emulator is powered by 
-                    <a href="https://github.com/copy/v86" target="_blank" rel='noreferrer'> v86 </a> 
+                    <a href="https://github.com/copy/v86" target="_blank" rel='noreferrer'> v86</a>.
                 </div>
                 <div> The website is developed with 
-                    <a href='https://react.dev/' target="_blank" rel='noreferrer'> ReactJS </a>
+                    <a href='https://react.dev/' target="_blank" rel='noreferrer'> ReactJS</a>.
                 </div>
                 <div> 
-                    <a href="https://www.figma.com/" target="_black" rel="noreferrer" >Figma</a> is our best friend for designing the website </div>
-                <div>The designer and the developer of the website can be found at the footer of website {`:)`}</div>
+                    <a href="https://www.figma.com/" target="_black" rel="noreferrer" >Figma</a> is our best friend for designing the website.
+                </div>
             </div>
             <div>
                 <h3>Something else...</h3>
-                <div>Hey there! The designer/developer is here!</div>
+                <div>Hey there! The designer and developer, Dongming (or MeanPaper) is here!</div>
                 <div>The styling of the website is inspired by the styling used on 
-                    <a href="https://vitejs.dev/" target="_blank" rel="noreferrer"> vite </a> (a great tool for React App but it gives some trouble when developing the website)
+                    <a href="https://vitejs.dev/" target="_blank" rel="noreferrer"> vite </a> (a great tool for React App but it gives some trouble when developing the website).
                 </div>
-                <div>The logo of the website is inspired by the logo of C++ </div>
-                
+                <div>The team/website logo is inspired by the logo of C++. </div>
             </div>
         </>
     );
